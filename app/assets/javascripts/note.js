99a747df642634a29ce078e2dcecf0b86a347c34
bradeckert <beckert@mit.edu>
2014-11-06 16:53:46 -0500

    More note/sequence

--- app/assets/javascripts/note.js
+++ app/assets/javascripts/note.js
@@ -1,2 +1,19 @@
 // this file will contain the representation of a note
 // will have a draw method
+
+var Note = function(note, duration) {
+    ////////////////////////////////////////////////
+    // Representation
+    //
+
+  this.note = note;
+  this.duration = duration;
+
+  ////////////////////////////////////////////////
+  // Public methods
+  //
+
+  this.draw = function(){
+    // TODO
+  }
+}
--- app/assets/javascripts/sequence.js
+++ app/assets/javascripts/sequence.js
@@ -1,2 +1,19 @@
 // this file will contain the representation of a sequence
 // must be able to draw itself by calling all Notes within it
+
+var Sequence = function(notes) {
+    ////////////////////////////////////////////////
+    // Representation
+    //
+
+  this.notes = notes;
+
+  ////////////////////////////////////////////////
+  // Public methods
+  //
+
+  this.draw = function(){
+    // TODO
+    // for note in notes...
+  }
+}
