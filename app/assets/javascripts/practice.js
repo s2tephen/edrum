b8a2549f13d201e1a9e334c4c24a6e474249d1d5
Stephen Suen <ssuen@mit.edu>
2014-12-07 00:29:24 -0500

    Added metronome.

--- app/assets/javascripts/practice.js
+++ app/assets/javascripts/practice.js
@@ -1,17 +1,28 @@
 $(document).ready(function() {
-  var tick = document.getElementsByTagName("audio")[0];
-  tick.addEventListener('ended', function() {
-    tick.currentTime = 0;
-    setTimeout(function() { tick.play(); }, 60000/sequence.bpm);
-  });
+  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  var timer;
+  var currTime = 0.0;
+
+  function scheduleTick() {
+    while(currTime < audioCtx.currentTime + 0.1) {
+      playNote(currTime);
+      currTime += 60.0 / sequence.bpm;
+    }
+    timer = window.setTimeout(schedule, 0.1);
+  }
+
+  function playTick(t) {
+    var oscillator = audioCtx.createOscillator();
+    oscillator.connect(audioCtx.destination);
+    oscillator.frequency.value = 380;
+    oscillator.start(t);
+    oscillator.stop(t + 0.05);
+  }
 
   $("#practice-play-btn").click(function(e) {
     var id = sequence.id;
     e.stopImmediatePropagation();
     $.post('/practice/'+id+'/start', function(data) {});
-    tick.play();
+    scheduleTick();
   });
 });
-
-
-
